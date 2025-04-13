73e4b6d96c7240ed6b3fb13de62cbe0a62fa4a44
Izzatbek <abdusharipovizzat03@gmail.com>
2025-04-13 22:51:32 +0500

    admin panel

--- server/src/main/java/com/admin/server/model/Token.java
+++ server/src/main/java/com/admin/server/model/Token.java
@@ -1,9 +1,13 @@
 package com.admin.server.model;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -22,8 +26,13 @@ public class Token {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @Column(nullable = false,unique = true)
+    private String refreshToken;
+
+    @OneToOne//user bilan bog'langan id orqali bu yerda faqat userni id qismi saqlandi
+    @JoinColumn(name = "user_id",referencedColumnName = "id")//o'sha ustunga qo'shilib olish
     private User user;
 
-    private String refreshToken;
+    @Column(nullable = false)
+    private Date expiryDate;
 }
--- server/src/main/java/com/admin/server/util/JWTUtil.java
+++ server/src/main/java/com/admin/server/util/JWTUtil.java
@@ -1,3 +1,72 @@
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.admin.server.model.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class JWTUtil {
-    
+    private static final String secret=Base64.getEncoder().encodeToString("uzbekistanEpisode1234567890abcdef1234567890".getBytes(StandardCharsets.UTF_8));
+    private static final long accessTime=15*60*1000;
+    private static final long refreshTime=24*60*60*1000;
+
+    public Key getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(User user,long expirationTime){
+        return Jwts.builder()
+        .setSubject(user.getUsername())//1
+        .claim("email", user.getEmail())//2
+        .claim("role", user.getRole())//3 ushbu malumotlarni qabul qiladi tokenning payload qismi
+        .setIssuedAt(new Date())//tokenni chiqarilgan vaqti
+        .setExpiration(new Date(System.currentTimeMillis()+expirationTime))//amal qilish muddati
+        .signWith(getSigningKey(),SignatureAlgorithm.HS256)//imzo HS256
+        .compact();
+    }
+        //tokenlar access va refresh
+        public Map<String,String> generateTokens(User user){
+            Map<String,String> tokens=new HashMap<>();
+            tokens.put("accessToken", generateToken(user, accessTime));
+            tokens.put("refreshToken", generateToken(user, refreshTime));
+            return tokens;
+        }
+        public boolean validateToken(String token){
+            try {
+                Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;  
+            } catch (Exception e) {
+                return false;
+            }
+           
+        }
+        public String extractUsername(String username){
+            return Jwts.parserBuilder()
+            .setSigningKey(getSigningKey())
+            .build()
+            .parseClaimsJws(username)
+            .getBody()
+            .getSubject();
+        }
+
+        public String extractEmail(String email){
+            return Jwts.parserBuilder()
+            .setSigningKey(getSigningKey())
+            .build()
+            .parseClaimsJws(email)
+            .getBody()
+            .getSubject();
+        }
 }
